bd0161e5b1853c376f88e6b4ab725dfb5fab95b1
moscowmovsta <93486151+moscowmovsta@users.noreply.github.com>
2022-08-24 13:24:07 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -324,7 +324,7 @@
 
 function makeTimer() {
 
-		var endTime = new Date("21 August 2022 9:56:00 GMT+01:00");			
+		var endTime = new Date("01 November 2022 9:56:00 GMT+01:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
